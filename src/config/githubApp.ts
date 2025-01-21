0f096ddd8245a49d52fa8f86b5ba643efa90b2a7
miracleonyenma <miracleficient@gmail.com>
2025-01-21 08:21:54 +0100

    feat: refactor GitHub app configuration to use GITHUB_PRIVATE_KEY environment variable

--- src/config/githubApp.ts
+++ src/config/githubApp.ts
@@ -2,16 +2,12 @@ import { config } from "dotenv";
 
 import { App } from "octokit";
 import { createNodeMiddleware } from "@octokit/webhooks";
-import fs from "fs";
 
 config();
 
 const appId = process.env.GITHUB_APP_ID;
 const webhookSecret = process.env.GITHUB_WEBHOOK_SECRET;
-const privateKeyPath = process.env.PRIVATE_KEY_PATH;
 const privateKey = process.env.GITHUB_PRIVATE_KEY;
-
-// const privateKey = fs.readFileSync(privateKeyPath, "utf8");
 
 const githubApp = new App({
   appId: appId,
